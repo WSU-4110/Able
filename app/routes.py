6e6e2ddb337ff8f50df4306ca547285b6afa62cf
KennyGajefski <kenneth.ag@outlook.com>
2019-09-28 15:11:41 -0400

    Bootstrap integration testing branch.

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 # Can't really explain what this does technically, but it works ¯\_(ツ)_/¯
-app = Flask(__name__)
+# Instantiates the Flask app I think?
+app = Flask(__name__, template_folder='templates')
+Bootstrap(app)
 
 # Root directory route. This will always be the first page to load.
 @app.route('/')
-def hello_world():
+def main_page():
     # Telling it what to render out. When it gets more complex, we will be passing HTML templates within these functions
-    return 'Hello World!'
+    return render_template('main.html')
 
 
 # Can't really explain what this does technically, but it works ¯\_(ツ)_/¯
+# I just now this makes it able to run
 if __name__ == '__main__':
     app.run()
