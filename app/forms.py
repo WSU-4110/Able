a3992024fc45ac5a631023de59e05f4124dfc3fb
KennyGajefski <kenneth.ag@outlook.com>
2019-10-06 17:34:24 -0400

    Actually pushing the files necessary for the form

---
+++ app/forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, EqualTo
+
+
+class AccountCreation(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Create Account')
