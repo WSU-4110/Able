76bc402473109bcad5e4c86de7cd086ed7dca258
KennyGajefski <kenneth.ag@outlook.com>
2019-10-08 16:25:34 -0400

    Minor comment before merging

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@
 from app import db
 
 
+# We'll need to eventually design the whole Database, but this is fine enough now for the User table
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.VARCHAR(30))
