c3d80d487e4612c33b410596b2126792b43057e2
Dennis Zack <dzackhere@gmail.com>
2019-10-09 22:41:19 -0400

    Test commit

--- app/Notifications.py
+++ app/Notifications.py
@@ -11,7 +11,7 @@ body = 'This is a test email, please do not respond.'
 
 email_text = (sent_from, ", ".join(to), subject, body)  # create a single string from the given email components
 
-#have a try-catch statement in case of a faliure to send the message
+# Have a try-catch statement in case of a faliure to send the message
 # try:
 server = smtplib.SMTP_SSL('smtp.gmail.com') # Connect to the gmail server
 server.ehlo()  # identify ourselves to smtp gmail client
@@ -20,7 +20,7 @@ server.ehlo()  # re-identify ourselves as an encrypted connection
 # server.set_debuglevel(True)
 server.login(gmail_user, gmail_password)  # Attempt gmail login
 server.sendmail(sent_from, to, email_text)  # Send the message over the server
-server.close()  #Shut down the server connection
+server.close()  # Shut down the server connection
 
 print('Email sent!')  # Let the user know that their email was sent sussessfully
 
