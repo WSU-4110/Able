e167370f9edad093e372e951207a044711f14283
Dennis Zack <dzackhere@gmail.com>
2019-11-29 18:10:10 -0500

    Some minor updates.

--- app/testing/app_testing.py
+++ app/testing/app_testing.py
@@ -1,26 +1,25 @@
 from app import app
-from .models import *
-from .notifications import Email
+from app.models import *
 import unittest
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-class FlaskTests(unittest.TestCase):
+class PyCharmTests(unittest.TestCase):
 
-    # Runs before every test
-    def setUp(self):
+    # executed prior to each test
+    def test_setUp(self):
         app.config['TESTING'] = True
         app.config['WTF_CSRF_ENABLED'] = False
         app.config['DEBUG'] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or \
-                                                'sqlite:///' + os.path.join(basedir, 'test.db')
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+                                                os.path.join(app.config['BASEDIR'], 'able.db')
         self.app = app.test_client()
         db.drop_all()
         db.create_all()
 
-    # Runs after every test
+    # executed after each test
     def tearDown(self):
         pass
 
