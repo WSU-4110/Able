4b4da9b165a686a7e22f04db11f9c67ba8abe22c
Dennis Zack <dzackhere@gmail.com>
2019-11-29 18:45:38 -0500

    Some minor updates.

--- app/app_testing.py
+++ app/app_testing.py
@@ -24,8 +24,8 @@ class FlaskTests(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_email_sending(self):
-        response = self.app.get('/send_email_button', follow_redirects=True)
+    def test_account_creation_email_sending(self):
+        response = self.app.get('/register', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
     def test_user_profile_switch(self):
@@ -36,9 +36,17 @@ class FlaskTests(unittest.TestCase):
         response = self.app.get('/return_to_menu', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
+    def test_write_new_review(self):
+        response = self.app.get('/write', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
     def test_show_new_reviews(self):
         response = self.app.get('/show_reviews', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
-        
+
+    def test_email_sending_button(self):
+        response = self.app.get('/send_email_button', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
 if __name__ == '__main__':
     unittest.main()
