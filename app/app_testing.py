f1f3e13f6394fc71180b633123b56c28f11a1522
Dennis Zack <dzackhere@gmail.com>
2019-11-29 18:29:45 -0500

    Some minor updates.

---
+++ app/app_testing.py
@@ -0,0 +1,31 @@
+from app import app
+from .models import *
+import unittest
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class FlaskTests(unittest.TestCase):
+
+    # Runs before every test
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or \
+                                                'sqlite:///' + os.path.join(basedir, 'test.db')
+        self.app = app.test_client()
+        db.drop_all()
+        db.create_all()
+
+    # Runs after every test
+    def tearDown(self):
+        pass
+
+    def test_main_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
