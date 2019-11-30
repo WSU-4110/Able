f834a1d09702307d05a39aa0b18f136dd97d6251
Dennis Zack <dzackhere@gmail.com>
2019-11-29 19:25:43 -0500

    Commit of the third unit test.

--- app/app_testing.py
+++ app/app_testing.py
@@ -24,8 +24,8 @@ class FlaskTests(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_user_profile_switch(self):
-        response = self.app.get('/user_profile', follow_redirects=True)
+    def test_return_to_menu(self):
+        response = self.app.get('/return_to_menu', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
