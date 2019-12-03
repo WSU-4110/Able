30954895fbef889170271caab1bf05d24ab3a0e0
Dennis Zack <dzackhere@gmail.com>
2019-12-03 15:07:41 -0500

    Minor code changes and improvements.

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,20 @@
 # This will hold any and all form templates
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField,TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, EqualTo, ValidationError
 from app.models import User
 import smtplib
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from app.models import Reviews
+
+
+class Contact(FlaskForm):
+    name = StringField('Name', validators=[DataRequired("Please enter your name.")])
+    email = StringField('Email', validators=[DataRequired("Please enter your email address.")])
+    subject = StringField('Subject', validators=[DataRequired("Please enter a subject.")])
+    message = StringField('Message', validators=[DataRequired("Please enter a message.")])
+    submit = SubmitField("Send")
 
 
 class AccountCreation(FlaskForm):
@@ -25,20 +33,21 @@ class AccountCreation(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please use a different email address.')
-        else:
-            try:
-                server = smtplib.SMTP_SSL('smtp.gmail.com', 465)  # Establishes a connection with the gmail server
-                server.login('able4110group@gmail.com', 'able12345')  # Accesses the server function to prompt login
-                server.sendmail(
-                    'able4110group@gmail.com',
-                    'able4110group@gmail.com',
-                    "Test Message, Do Not Respond")  # Accesses the server function to send the email
-                server.quit()  # Exits out of the server so there is no trailing connection
-                print('Email sent!')  # Let the user know that their email was sent sussessfully
 
-            except:
-                print('Something went wrong...')
+class Login(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
 
 class ReviewCreation(FlaskForm):
     review = TextAreaField('Review', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+"""
+class ReviewDisplay():
+    def display_database_reviews(self):
+        engine = create_engine('sqlite:///:memory:', echo=True)
+        session = sessionmaker(bind=engine)
+        records = session.query(Customer).filter(Customer.first_name == 'Carl').all()
+"""
